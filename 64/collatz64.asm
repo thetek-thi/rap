d89eaeb1f8991a60c01f9210cbaedbf40db4e367
thetek-thi <git-thi@thetek.de>
2021-11-24 12:35:09 +0100

    working 64 bit collatz

--- 64/collatz64.asm
+++ 64/collatz64.asm
@@ -1,30 +1,88 @@
     global  main
     
-    extern  printf
     extern  atoi
+    extern  exit
+    extern  printf
 
 
 
 section .data
-    strf:   db '%s',10,0
+    strf:   db '%d',10,0
+    eargc:  db 'Usage: `collatz64 <n>`',10,0
+    res:    dq 1
 
 
 
 section .text
 main:
-    push    rdi
-    push    rsi
-    sub     rsp, 8
+    ; no command line arguments -> print error and exit
+    add     rsi, 8
+    dec     rdi
+    jz      _eargc
 
+    ; get number from first argument in rax
+    push    rbp
+    mov     rdi, [rsi]
+    call    atoi
+    pop     rbp
+
+    ; call collatz function
+    mov     [res], rax
+    call    _collatz
+
+    xor     rax, rax
+    ret
+
+
+
+_collatz:
+    call    _printrax
+    __start:
+        mov     rax, [res]
+        shl     rax, 63
+        shr     rax, 63
+        jz      __even
+    __odd:
+        mov     rax, [res]
+        mov     rdx, 3
+        mul     rdx
+        inc     rax
+        mov     [res], rax
+        call    _printrax
+        jmp     __end
+    __even:
+        mov     rax, [res]
+        mov     rbx, 2
+        xor     rdx, rdx
+        div     rbx
+        mov     [res], rax
+        call    _printrax
+        jmp     __end
+    __end:
+        mov     rax, [res]
+        dec     rax
+        jnz     __start
+
+    ret
+
+
+
+_printrax:
     push    rbp
     mov     rdi, strf
-    mov     rsi, [rsi]
-    xor     rax, rax
+    mov     rsi, rax
+    call    printf
+    pop     rbp
+    ret
+
+
+
+_eargc:
+    push    rbp
+    mov     rdi, eargc
     call    printf
     pop     rbp
 
-    add     rsp, 8
-
-    xor     eax, eax
-    ret
+    mov     rdi, 1
+    call    exit
 
