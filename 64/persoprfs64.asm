7817d9a4979db5b0c34c110ae77d110ecaa0188e
thetek-thi <git-thi@thetek.de>
2021-12-02 08:26:58 +0100

    add 64bit version of p4

--- 64/persoprfs64.asm
+++ 64/persoprfs64.asm
@@ -6,23 +6,21 @@
 
 
 
-	section .data
+	section	.data
 
-fgmsg:	db	'perso number > ',0
+prompt:	db	'perso number > ',0
 pnr:	db	0,0,0,0,0,0,0,0,0,0,0
-pnrx:	dw	0,0,0,0,0,0,0,0,0,0
-fmtint:	db	'%c',10,0
+fmtint:	db	'%d',10,0
 
 
 
-	section .text
+	section	.text
 
 main:	push	rbp
-	mov	rdi,	fgmsg
+	mov	rdi,	prompt
 	call	printf
 	pop	rbp
 
-	; please enter a valid perso number because i ain't doing some validation here
 	push	rbp
 	mov	rdi,	pnr
 	mov	rsi,	11
@@ -30,18 +28,98 @@ main:	push	rbp
 	call	fgets
 	pop	rbp
 
+	mov	[pnr+9],	byte ' '
+
 	push	rbp
 	mov	rdi,	pnr
 	call	printf
 	pop	rbp
 
-	push	rbp
 	call	testpn
-	pop	rbp
 
 	ret
 
 
 
-testpn:	ret
+testpn:	xor	rcx,	rcx
+ _loop:	mov	rax,	[pnr+rcx]
+	and	rax,	0xFF ; mask that only keeps the last byte
+
+	sub	rax,	48
+	cmp	rax,	10
+	jl	_skip
+	sub	rax,	7
+ _skip:	mov	[pnr+rcx],	al
+	
+	inc	rcx
+	cmp	rcx,	10
+	jne	_loop
+; end _loop
+
+	xor	rax,	rax
+
+	
+	mov	rdx,	[pnr+0]
+	and	rdx,	0xFF
+	imul	rdx,	7
+	add	rax,	rdx
+
+	mov	rdx,	[pnr+1]
+	and	rdx,	0xFF
+	imul	rdx,	3
+	add	rax,	rdx
+
+	mov	rdx,	[pnr+2]
+	and	rdx,	0xFF
+	add	rax,	rdx
+
+
+	mov	rdx,	[pnr+3]
+	and	rdx,	0xFF
+	imul	rdx,	7
+	add	rax,	rdx
+
+	mov	rdx,	[pnr+4]
+	and	rdx,	0xFF
+	imul	rdx,	3
+	add	rax,	rdx
+
+	mov	rdx,	[pnr+5]
+	and	rdx,	0xFF
+	add	rax,	rdx
+
+
+	mov	rdx,	[pnr+6]
+	and	rdx,	0xFF
+	imul	rdx,	7
+	add	rax,	rdx
+
+	mov	rdx,	[pnr+7]
+	and	rdx,	0xFF
+	imul	rdx,	3
+	add	rax,	rdx
+
+	mov	rdx,	[pnr+8]
+	and	rdx,	0xFF
+	add	rax,	rdx
+
+	mov	rbx,	10
+	mov	rcx,	rax
+	xor	rdx,	rdx
+	div	rbx
+	mov	rax,	rdx
+
+	call	printrax
+
+	ret
+
+
+
+printrax:
+	push	rbp
+	mov	rdi,	fmtint
+	mov	rsi,	rax
+	call	printf
+	pop	rbp
+	ret
 
